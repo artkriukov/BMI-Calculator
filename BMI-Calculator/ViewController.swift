117e4535c83d8dae28ef68e358a16f65d183a6fb
Artem Kriukov <art.kriukov@gmail.com>
2024-12-17 19:37:16 +0300

    Implemented UI for weight and height adjustment sliders. Added @IBAction methods to update weight and height values based on slider changes

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightUILabel: UILabel!
+    @IBOutlet weak var weightUILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func heightValueChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightUILabel.text = "\(height)m"
+    }
 
+    @IBAction func weightValueChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightUILabel.text = "\(weight)Kg"
+    }
 }
 
