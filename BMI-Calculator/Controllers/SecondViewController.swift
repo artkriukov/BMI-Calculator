d08200ee907062789ace2a73e3745c65a1c90607
Artem Kriukov <art.kriukov@gmail.com>
2024-12-18 17:23:28 +0300

    the logic of transition between two View Controllers has been configured

--- BMI-Calculator/Controllers/SecondViewController.swift
+++ BMI-Calculator/Controllers/SecondViewController.swift
@@ -5,3 +5,20 @@
 //  Created by Artem on 12/18/24.
 //
 
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    override func viewDidLoad() {
+        
+        view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
+        
+    }
+}
