156f509f842b9e7d95d6b00e0a0278a188213b96
Artem Kriukov <art.kriukov@gmail.com>
2024-12-19 19:06:47 +0300

    - Added  to define the BMI structure with value, advice, and color properties. - Added  to handle BMI calculation, provide advice, and determine background color. - Implemented BMI calculation logic based on height and weight. - Updated 2nd screen to display:   - Calculated BMI value.   - Health advice.   - Dynamic background color based on BMI range.

--- BMI-Calculator/Controllers/CalculateViewController.swift
+++ BMI-Calculator/Controllers/CalculateViewController.swift
@@ -9,42 +9,42 @@ import UIKit
 
 class CalculateViewController: UIViewController {
     
-    var bmiValue = "0.0"
+    private var calculatorBrain = CalculatorBrain()
     
-    @IBOutlet weak var heightUILabel: UILabel!
-    @IBOutlet weak var weightUILabel: UILabel!
-    @IBOutlet weak var heightSlider: UISlider!
-    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet private weak var heightUILabel: UILabel!
+    @IBOutlet private weak var weightUILabel: UILabel!
+    @IBOutlet private weak var heightSlider: UISlider!
+    @IBOutlet private weak var weightSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-    @IBAction func heightValueChanged(_ sender: UISlider) {
+    @IBAction private func heightValueChanged(_ sender: UISlider) {
         let height = String(format: "%.2f", sender.value)
         heightUILabel.text = "\(height)m"
     }
 
-    @IBAction func weightValueChanged(_ sender: UISlider) {
+    @IBAction private func weightValueChanged(_ sender: UISlider) {
         let weight = String(format: "%.0f", sender.value)
         weightUILabel.text = "\(weight)Kg"
     }
     
-    @IBAction func calculatePressed(_ sender: UIButton) {
+    @IBAction private func calculatePressed(_ sender: UIButton) {
         let height = heightSlider.value
         let weight = weightSlider.value
         
-        let bmi = weight / pow(height, 2)
-        bmiValue = String(format: "%.1f", bmi)
-        
+        calculatorBrain.calculateBMI(height: height, weight: weight)
         self.performSegue(withIdentifier: "goToResult", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResult" {
             let destinationVC = segue.destination as! ResultViewController
-            destinationVC.bmiValue = bmiValue
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
         }
     }
 }
